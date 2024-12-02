bfa5956910e4f2a154c7712ca855fd6efb38a709
ishaanam <ishaana.misra@gmail.com>
2024-12-02 15:31:54 -0500

    add prompt

--- main.c
+++ main.c
@@ -9,7 +9,13 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+void prompt() {
+    char buffer[256];
+    getcwd(buffer, 255);
+    printf("%s $ ", buffer);
+    fflush(stdout);
+}
+
 int main(int argc, char *argv[]) {
-    
     return 0;
 }
