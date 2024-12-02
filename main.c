bfadcdac331c8bd841116dbfe06bda0ac4ad690d
ishaanam <ishaana.misra@gmail.com>
2024-12-02 15:16:57 -0500

    add basic main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
