b5be656f80b186d2e821e80fb845d7a5f06cef3e
Sasha Murokh <smurokh50@stuy.edu>
2024-12-03 14:24:45 -0600

    functionality

--- main.c
+++ main.c
@@ -16,19 +16,21 @@ void prompt() {
     fflush(stdout);
 }
 
-void input() {
-  char user_input[256];
-  fgets(user_input, 255, stdin);
-  char* temp;
-  char * input;
-  strcpy(input,user_input);
-  while (temp = strsep( &input, ";" )){
-    printf("%s\n", temp);
-  }
-}
-
 int main(int argc, char *argv[]) {
     prompt();
-    input();
+    char user_input[256];
+    fgets(user_input, 255, stdin);
+    if (user_input==NULL){ //exits if end of file
+      exit(0);
+    }
+    char* temp;
+    char * input;
+    strcpy(input,user_input);
+    while (temp = strsep( &input, ";" )){
+      printf("%s\n", temp);
+      int status1;
+      pid_t commands;
+      commands = wait(&status1);
+    }
     return 0;
 }
