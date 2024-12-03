fb211f401267c22dd7bdaaef3c6f1385bb401a3a
ishaanam <ishaana.misra@gmail.com>
2024-12-03 15:25:24 -0500

    add prompt to shell functions

--- main.c
+++ main.c
@@ -1,20 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <time.h>
-#include <sys/types.h>
-#include <dirent.h>
-
-void prompt() {
-    char buffer[256];
-    getcwd(buffer, 255);
-    printf("%s $ ", buffer);
-    fflush(stdout);
-}
+#include "shell_functions.h"
 
 int main(int argc, char *argv[]) {
     prompt();
--- shell_functions.h
+++ shell_functions.h
@@ -1,4 +1,17 @@
 #ifndef shell_functions_h
 #define shell_functions_h
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+void prompt();
+
 #endif
