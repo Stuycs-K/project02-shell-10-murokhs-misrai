bfac7bc087e48980cefe6abf68816753057755a1
Sasha Murokh <smurokh50@stuy.edu>
2024-12-04 13:12:20 -0600

    fixed seg

--- main.c
+++ main.c
@@ -3,18 +3,17 @@
 int main(int argc, char *argv[]) {
     prompt();
     char user_input[256];
-    fgets(user_input, 255, stdin);
+
     if (user_input==NULL){ //exits if end of file
       exit(0);
     }
     char* temp;
-    char * input;
-    strcpy(input,user_input);
+    char * input  = fgets(user_input, 255, stdin);
     while (temp = strsep( &input, ";" )){
       printf("%s\n", temp);
-      int status1;
-      pid_t commands;
-      commands = wait(&status1);
+      //int status1;
+      //pid_t commands;
+      //commands = wait(&status1);
     }
     return 0;
 }
