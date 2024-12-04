d432adc0a3c453b43560d3b6fd8107d15975ee15
Sasha Murokh <smurokh50@stuy.edu>
2024-12-04 13:26:49 -0600

    args

--- main.c
+++ main.c
@@ -10,10 +10,18 @@ int main(int argc, char *argv[]) {
     char* temp;
     char * input  = fgets(user_input, 255, stdin);
     while (temp = strsep( &input, ";" )){
-      printf("%s\n", temp);
-      char * args[100];
-      parse_args(temp, args);
-
+      if (strcmp(temp,"exit")==0){
+        printf("exiting\n");
+        kill(getpid(), SIGINT);
+      }
+      pid_t child;
+      child = fork();
+      if (child==0){
+        char * args[256];
+        parse_args(temp,args);
+        execvp(args[0],args);
+      }
+    }
       //int status1;
       //pid_t commands;
       //commands = wait(&status1);
