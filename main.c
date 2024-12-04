a599bfd1567b8210470ec6b27760753014f31b0f
Sasha Murokh <smurokh50@stuy.edu>
2024-12-04 13:46:13 -0600

    fork

--- main.c
+++ main.c
@@ -1,29 +1,32 @@
 #include "shell_functions.h"
 
-int main(int argc, char *argv[]) {
-    //prompt();
-    char user_input[256];
+static void sighandler(int signo) {
 
+}
+
+int main(int argc, char *argv[]) {
+    prompt();
+    char user_input[256];
     if (user_input==NULL){ //exits if end of file
       exit(0);
     }
     char* temp;
     char * input  = fgets(user_input, 255, stdin);
     while (temp = strsep( &input, ";" )){
-      if (strcmp(temp,"exit")==0){
-        printf("exiting\n");
-        kill(getpid(), SIGINT);
+      char * args[256];
+      parse_args(temp,args);
+      if (strcmp(args[0],"exit")==0){
+        exit(0);
       }
       pid_t child;
       child = fork();
       if (child==0){
-        char * args[256];
-        parse_args(temp,args);
         execvp(args[0],args);
       }
     }
-      //int status1;
-      //pid_t commands;
-      //commands = wait(&status1);
+    int status1;
+    pid_t commands;
+    commands = wait(&status1);
+    main(argc,argv);
     return 0;
 }
