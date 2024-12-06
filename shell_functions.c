8aa7cd4448c7f008cde928dcd16efbdf6af0c077
Sasha Murokh <smurokh50@stuy.edu>
2024-12-06 14:17:04 -0600

    if input is empty

--- shell_functions.c
+++ shell_functions.c
@@ -19,35 +19,40 @@ void parse_args( char * line, char ** arg_ary ) {
 
 //returns 0 if passed, -1 if ended
 int take_input(){
-  prompt();
   char user_input[256];
-  char* temp;
-  char * input  = fgets(user_input, 255, stdin);
-  if (user_input==NULL){ //exits if end of file
-    exit(0);
-    return -1;
-  }
-  user_input[strlen(user_input)-1] = '\0'; //fix newline
-  while (temp = strsep( &input, ";" )){
-    char * args[256];
-    parse_args(temp,args);
-    if (strcmp(args[0],"exit")==0){
+  char * input;
+  prompt();
+  while(input = fgets(user_input, 255, stdin)){
+    char* temp;
+    if (user_input==NULL){ //exits if end of file
       exit(0);
       return -1;
     }
-    if (strcmp(args[0],"cd")==0){
-      chdir(args[1]);
+    if (strcmp(user_input,"\n")==0){
+      break;
     }
-    else{
-      pid_t child;
-      child = fork();
-      if (child==0){
-        execvp(args[0],args);
+    user_input[strlen(user_input)-1] = '\0'; //fix newline
+    while (temp = strsep( &input, ";" )){
+      char * args[256];
+      parse_args(temp,args);
+      if (strcmp(args[0],"exit")==0){
+        exit(0);
+        return -1;
+      }
+      if (strcmp(args[0],"cd")==0){
+        chdir(args[1]);
+      }
+      else{
+        pid_t child;
+        child = fork();
+        if (child==0){
+          execvp(args[0],args);
+        }
       }
     }
+    int status1;
+    pid_t commands;
+    commands = wait(&status1);
+    return 0;
   }
-  int status1;
-  pid_t commands;
-  commands = wait(&status1);
-  return 0;
 }
