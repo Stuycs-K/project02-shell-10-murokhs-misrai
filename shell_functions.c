bf7c459f4028521fa9513b17642df36d64ddd6c4
Sasha Murokh <smurokh50@stuy.edu>
2024-12-08 23:57:03 -0500

    fixed exit code !!

--- shell_functions.c
+++ shell_functions.c
@@ -22,37 +22,36 @@ int take_input(){
   char user_input[256];
   char * input;
   prompt();
-  while(input = fgets(user_input, 255, stdin)){
-    char* temp;
-    if (user_input==NULL){ //exits if end of file
+  input = fgets(user_input, 255, stdin);
+  if (input==NULL){ //exits if end of file
+    printf("end of file\n");
+    exit(0);
+  }
+  if (strcmp(user_input,"\n")==0){ //if just enter is pressed
+    return 0;
+  }
+  char* temp;
+  user_input[strlen(user_input)-1] = '\0'; //fix newline
+  while (temp = strsep( &input, ";" )){
+    char * args[256];
+    parse_args(temp,args);
+    if (strcmp(args[0],"exit")==0){
       exit(0);
       return -1;
     }
-    if (strcmp(user_input,"\n")==0){
-      break;
+    if (strcmp(args[0],"cd")==0){
+      chdir(args[1]);
     }
-    user_input[strlen(user_input)-1] = '\0'; //fix newline
-    while (temp = strsep( &input, ";" )){
-      char * args[256];
-      parse_args(temp,args);
-      if (strcmp(args[0],"exit")==0){
-        exit(0);
-        return -1;
-      }
-      if (strcmp(args[0],"cd")==0){
-        chdir(args[1]);
-      }
-      else{
-        pid_t child;
-        child = fork();
-        if (child==0){
-          execvp(args[0],args);
-        }
+    else{
+      pid_t child;
+      child = fork();
+      if (child==0){
+        execvp(args[0],args);
       }
     }
-    int status1;
-    pid_t commands;
-    commands = wait(&status1);
-    return 0;
   }
+  int status1;
+  pid_t commands;
+  commands = wait(&status1);
+  return 0;
 }
